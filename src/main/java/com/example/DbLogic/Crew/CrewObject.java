79d0524650bb9fe7ec7a8f8776468b9c2c675eb9
jason <gapspace@engineer.com>
2021-06-26 17:07:45 +0100

    Gathering cast and crew data

--- src/main/java/com/example/DbLogic/Crew/CrewObject.java
+++ src/main/java/com/example/DbLogic/Crew/CrewObject.java
@@ -1,4 +1,32 @@
 package com.example.DbLogic.Crew;
 
 public class CrewObject {
+
+    private String tconst;
+    private String directors;
+    private String writers;
+
+    public String getTconst() {
+        return tconst;
+    }
+
+    public void setTconst(String tconst) {
+        this.tconst = tconst;
+    }
+
+    public String getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(String directors) {
+        this.directors = directors;
+    }
+
+    public String getWriters() {
+        return writers;
+    }
+
+    public void setWriters(String writers) {
+        this.writers = writers;
+    }
 }
--- src/main/java/com/example/DbLogic/Crew/CrewRowMapper.java
+++ src/main/java/com/example/DbLogic/Crew/CrewRowMapper.java
@@ -1,4 +1,28 @@
 package com.example.DbLogic.Crew;
 
-public class CrewRowMapper {
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CrewRowMapper implements RowMapper<CrewObject> {
+
+    @Override
+    public CrewObject mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+
+        // Get the result set data
+        String tconst = resultSet.getString("tconst");
+        String directors = resultSet.getString("directors");
+        String writers = resultSet.getString("writers");
+
+        // ActorObject object data encapsulated
+        CrewObject crewObject = new CrewObject();
+
+        // Set the values
+        crewObject.setTconst(tconst);
+        crewObject.setDirectors(directors);
+        crewObject.setWriters(writers);
+
+        return crewObject;
+    }
 }
