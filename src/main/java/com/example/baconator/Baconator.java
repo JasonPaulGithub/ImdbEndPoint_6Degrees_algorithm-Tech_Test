a72c782b369b70f92f21d5be5548f4b0761c385d
jason <gapspace@engineer.com>
2021-06-27 12:53:44 +0100

    Added BFS research.
    // TODO: The Brute Baconator
    // TODO: Refactor, i.e combine findByPrimaryTitle and findByOriginalTitle
    // TODO: Apply comments

---
+++ src/main/java/com/example/baconator/Baconator.java
@@ -0,0 +1,24 @@
+package com.example.baconator;
+
+public class Baconator {
+    public Baconator(String name) {
+
+        /*
+        * create 10 threads
+        *
+        * thread operations:
+        *
+        * Set <String movieCheckedList>
+        * Set <String actorCheckedList>
+
+        * load root placeholder
+        * if placeholder != kevin bacon then
+        * get list of movies for placeholder
+        * select random movie not on checkedList, select random actor not on checkedList
+        * repeat cycle
+        * if no actors left, add movie to checked list, return to parent node
+        *
+        * */
+
+    }
+}
---
+++ src/main/java/com/example/baconator/composite/menu/Waitress.java
@@ -0,0 +1,13 @@
+package com.example.baconator.composite.menu;
+  
+public class Waitress {
+	MenuComponent allMenus;
+ 
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+ 
+	public void printMenu() {
+		allMenus.print();
+	}
+}
